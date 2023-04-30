a1ad0e44809ba07690bc4a3434079044fd84b91e
xeusteerapat <xeus085@gmail.com>
2023-04-30 13:20:07 +0700

    :white_check_mark: [Update] exercise

--- 01-curry.js
+++ 01-curry.js
@@ -13,7 +13,8 @@ const fromPair = fn => (x, y) => fn([x, y]);
 const res2 = fromPair(toPair(add))(3, 4);
 console.log(res2); // 7
 
-const curry = fn => x => y => fn(x, y);
+// const curry = fn => x => y => fn(x, y);
+const { curry } = require('ramda');
 
 const modulo = curry((x, y) => y % x);
 
@@ -28,3 +29,16 @@ const getOdds = filter(isOdd);
 
 const res4 = getOdds([1, 2, 3, 4, 5, 6, 7, 8]); // [ 1, 3, 5, 7 ]
 console.log(res4);
+
+const replace = curry((regex, replacement, str) =>
+  str.replace(regex, replacement)
+);
+
+const replaceVowels = replace(/[AEIOU]/gi, '!');
+const res5 = replaceVowels('Hey I am a programmer');
+console.log(res5);
+
+// Split
+const split = curry((delimeter, str) => str.split(delimeter));
+const batmanSplitted = split(' ', 'Jingle bells Batman smells');
+console.log(batmanSplitted);
