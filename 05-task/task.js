df02de98b99aa21db7398bb642d180d6446c9eed
xeusteerapat <xeus085@gmail.com>
2023-05-06 09:47:09 +0700

    :sparkles: [Add] add task

---
+++ 05-task/task.js
@@ -0,0 +1,39 @@
+/**
+ * Task
+ */
+const Task = fork => ({
+  fork,
+  ap: other =>
+    Task((rej, res) => fork(rej, f => other.fork(rej, x => res(f(x))))),
+  map: f => Task((rej, res) => fork(rej, x => res(f(x)))),
+  chain: f => Task((rej, res) => fork(rej, x => f(x).fork(rej, res))),
+  concat: other =>
+    Task((rej, res) =>
+      fork(rej, x =>
+        other.fork(rej, y => {
+          console.log('X', x, 'Y', y);
+          res(x.concat(y));
+        })
+      )
+    ),
+  fold: (f, g) =>
+    Task((rej, res) =>
+      fork(
+        x => f(x).fork(rej, res),
+        x => g(x).fork(rej, res)
+      )
+    ),
+});
+Task.of = x => Task((rej, res) => res(x));
+Task.rejected = x => Task((rej, res) => rej(x));
+Task.fromPromised =
+  fn =>
+  (...args) =>
+    Task((rej, res) =>
+      fn(...args)
+        .then(res)
+        .catch(rej)
+    );
+
+const taskOfTwo = Task.of(2).map(two => two);
+console.log(taskOfTwo);
