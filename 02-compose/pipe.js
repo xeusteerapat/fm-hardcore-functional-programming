7208501529e706c57ad44b9a6eee9999898f2845
xeusteerapat <xeus085@gmail.com>
2023-05-08 10:52:30 +0700

    :sparkles: [Update] update pipe function with recursive style

--- 02-compose/pipe.js
+++ 02-compose/pipe.js
@@ -17,10 +17,39 @@ const accumWithPipe = str => {
   return result(str);
 };
 
-module.exports = {
-  accumWithPipe,
+/**
+ *
+ * @param  {...functions} fns
+ * @returns function
+ */
+const recursivePipe = (...fns) => {
+  return value => {
+    if (fns.length === 0) {
+      return value;
+    }
+    const [fn, ...restFns] = fns;
+    return recursivePipe(...restFns)(fn(value));
+  };
+};
+
+const accumWithPipeRecursive = str => {
+  const result = recursivePipe(
+    str => str.toLowerCase(),
+    str => str.split(''),
+    arr => arr.map((item, idx) => ({ char: item, order: idx + 1 })),
+    arr => arr.map(item => item.char.repeat(item.order)),
+    arr => arr.map(item => item.replace(item[0], item[0].toUpperCase())),
+    arr => arr.join('-')
+  );
+
+  return result(str);
 };
 
 console.log(accumWithPipe('abcd'));
 console.log(accumWithPipe('RqaEzty'));
 console.log(accumWithPipe('cwAt'));
+
+module.exports = {
+  accumWithPipe,
+  accumWithPipeRecursive,
+};
